092ac7f28a7ddf3d66b3eee90840c6d8be038859
George Harrison <georgeharrison4242@gmail.com>
2020-07-03 22:38:53 +0100

    revamped boilerplate code for expressions for efficiency

--- expression.py
+++ expression.py
@@ -1,10 +1,13 @@
 class Expression:
-    # eval: Void -> Int
+    # eval: Void -> (Int,)
+    # evaluates expression to value v
+    # returns () if expression is invalid, and (v,) otherwise
     def eval():
         pass
 
-    # isValid: Void -> Bool
-    def isValid():
+    # __str__: Void -> String
+    # paranthesised string form of expression
+    def __str__():
         pass
 
 class Val(Expression):
@@ -12,53 +15,48 @@ class Val(Expression):
         self.value = value
 
     def eval(self):
-        if not self.isValid():
-            raise Exception("Cannot evaluate invalid expression")
-        return self.value
+        if self.value>0:
+            return (self.value,)
+        return ()
 
-    def isValid(self):
-        if self.value > 0:
-            return True
-        else:
-            return False
+    def __str__(self):
+        return str(self.value)
 
 class App(Expression):
     def __init__(self, op, l, r):
         self.op, self.l, self.r = op, l, r
 
     def eval(self):
-        if not self.isValid():
-            raise Exception("Cannot evaluate invalid expression") 
-        return self.op.apply(self.l.eval(), self.r.eval())
+        le, re = self.l.eval(), self.r.eval()
+        if len(le+re)==2: # if l and r both valid
+            (v,valid) = self.op.apply(le[0],re[0])
+            if valid: return (v,)
+        return ()
 
-    def isValid(self):
-        return (self.l.isValid() and
-               self.r.isValid() and
-               self.op.validOp(self.l.eval(),self.r.eval()))
-
+    def __str__(self):
+        return "(" + str(self.l) + str(self.op) + str(self.r) + ")"
 
 
 
 class Op:
-    # apply: Int, Int -> Int
+    # apply: Int, Int -> (Int,Bool)
+    # returns (v,valid) where valid is true if operation follows countdown rules
     def apply(l, r): pass
-
-    # validOp: Int, Int -> Bool
-    def validOp(l, r): pass
+    def __str__(): pass
 
 class Add(Op):
-    def apply(self, l, r): return l + r
-    def validOp(self, l, r): return True
+    def apply(self, l, r): return (l+r,l<=r)
+    def __str__(self): return "+"
 
 class Sub(Op):
-    def apply(self, l, r): return l - r
-    def validOp(self, l, r): return l > r
+    def apply(self, l, r): return (l-r,l>r)
+    def __str__(self): return "-"
 
 class Mul(Op):
-    def apply(self, l, r): return l * r
-    def validOp(self, l, r): return True
+    def apply(self, l, r): return (l*r,l!=1 and r!=1 and l<=r)
+    def __str__(self): return "x"
 
 class Div(Op):
-    def apply(self, l, r): return int(l / r)
-    def validOp(self, l, r): return l % r == 0
+    def apply(self, l, r): return (int(l/r),l%r==0 and r!=1)
+    def __str__(self): return "/"
 
